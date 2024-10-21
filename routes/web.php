0fd8e9c8652a7e5df0323018cc7b5d540e9f033b
Isuru Ekanayaka <133802550+isurulekanayaka@users.noreply.github.com>
2024-10-21 18:21:16 +0530

    login register UI done

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user.dashboard');
+    return view('auth.login');
 });
+
+Route::get('/index', function () {
+    return view('welcome');
+})->name('index');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
