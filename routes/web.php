a9781bbfc845a41a40e3c1a0e48282475847a00d
Gamika Punsisi <121822722+gamikapunsisi@users.noreply.github.com>
2024-10-21 18:45:59 +0530

    update view
    not complete

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('user.dashboard');
 });
+
+Route::get('/book-ticket', function () {
+    return view('user.book-ticket');
+})->name('book.ticket');
